5b8280a0ea80cb9d9110191a3d1ca89efea71418
gqln <gxg5186@psu.edu>
2018-07-29 12:39:07 -0700

    added comments to some questionable code 🐝

--- dance.js
+++ dance.js
@@ -63,35 +63,43 @@ function Bee(){
 		if(self.returning) {
 			var alpha = 0.95;
 
-			// Ah, I'm using these to measure progress along the line from the turnaround point to the initial point
+			// Ah, I'm using these to measure progress along the line from the turnaround point to the initial point, from 0 to 1
 			var progress = 1 - (Math.abs(self.lineX - self.initX)/self.distX + 
-								 Math.abs(self.lineY - self.initY)/self.distY)/2;
+								Math.abs(self.lineY - self.initY)/self.distY)/2;
 			self.lineX = self.lineX*alpha + self.initX*(1-alpha);
 			self.lineY = self.lineY*alpha + self.initY*(1-alpha);
 
+			// The bee changes direction towards its origin faster as it gets closer (makes more progress)
 			var omega = 0.95 - progress*(0.15);
 
 			var dx = self.x - self.initX;
 			var dy = self.y - self.initY;
 
+			// Points to the bee's origin
 			var rotation = Math.atan2(dy,dx) - Math.TAU/4;
 
 			var d2 = dx*dx + dy*dy;
+			// Kind of hacky, but this additional check avoids the bee "overshooting" its origin and forces it to return immediately
 			if (self.returnImmediately){
+				// Doesn't work as inteneded -- the goal was to gradually point towards the mouse when close to the origin
 				self.rotation = self.rotation*alpha + (1-alpha)*(Math.atan2(self.initY - Mouse.y,self.initX - Mouse.x) - Math.TAU/4 - rotation);
 				self.x = self.x*alpha + self.initX*(1-alpha);
 				self.y = self.y*alpha + self.initY*(1-alpha);
 			} else {
+				// Have the bee turn back around according to omega and move a bit in that direction
 				self.rotation = self.rotation*(omega) + (1 - omega)*(rotation);
 				self.x += Math.sin(self.rotation)*self.danceSpeed;
 				self.y -= Math.cos(self.rotation)*self.danceSpeed;
 			}
+			// If we're too close and nearly facing the origin
 			if(Math.abs(rotation - self.rotation) <= 0.05 && d2 < 10){
 				self.returnImmediately = true;
 			}
+			// If we're way too close (avoids unregulated oscillation lol)
 			if(d2<5){
 				self.x = self.initX;
 				self.y = self.initY;
+				// Again, doesn't work as inteneded (Line 84)
 				self.rotation = rotation*alpha + (1-alpha)*(Math.atan2(self.initY - Mouse.y,self.initX - Mouse.x) - Math.TAU/4 - rotation);
 				self.returnImmediately = false;
 				self.returning = false;
